5211e83b6cb9f8779a6fad79614ea90cad6ce904
rahat <rahatislamakash@gmail.com>
2022-08-10 21:29:35 +0600

    offline data storing in flutter

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -1,17 +1,17 @@
 import 'dart:convert';
-
+import 'dart:async';
 import 'package:http/http.dart' show Client;
 import '../models/item_model.dart';
 
 class NewsApiProvider{
   Client client = Client();
 
-  fetchTopIds() async{
+  Future<List<int>>fetchTopIds() async{
       final response = await client.get(Uri.parse('https://hacker-news.firebaseio.com/v0/topstories.json'));
       final ids = json.decode(response.body);
-      return ids;
+      return ids.cast<int>();
   }
-  fetchItem(int id) async{
+  Future<ItemModel>fetchItem(int id) async{
       final response = await client.get(Uri.parse('https://hacker-news.firebaseio.com/v0/item/$id.json'));
       final parsedJson = json.decode(response.body);
       return ItemModel.fromJson(parsedJson);
